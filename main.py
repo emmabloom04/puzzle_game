7ce004da53bec090bfd5455d253f49c29b675d01
emmabloom04 <blo23006@byui.edu>
2025-05-13 16:25:02 -0600

    putting down the base for game, nothing important yet

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # this will be the main file where code will run.
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Puzzle Game"
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+    def on_update(self, delta_time):
+        pass
+
+if __name__ == "__main__":
+    game = MyGame()
+    game.setup()
+    arcade.run()

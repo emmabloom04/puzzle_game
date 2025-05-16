2193ffcb5871f21d33156ec6966e46032b2a60dc
emmabloom04 <blo23006@byui.edu>
2025-05-15 18:20:45 -0600

    adding a background image, adding some notes

--- main.py
+++ main.py
@@ -7,16 +7,34 @@ SCREEN_TITLE = "Puzzle Game"
 
 class MyGame(arcade.Window):
     def __init__(self):
+        # constructor for the class
+        # will need to add variables here to store sprites and game data
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.background = None
 
     def setup(self):
-        pass
+        # method used to initalize or reset the game.
+        # typically you'd load sprites, set up the board, and initalize variables here.
+        # initializing background variable
+        self.background = arcade.load_texture("assets/table.jpg")
 
     def on_draw(self):
-        arcade.start_render()
+        # method automatically called to draw everything on screen.
+        # arcade.start_render()
+        self.clear()
+        # this line clears the screen and gets it ready for new drawings.
+        # after this you might draw sprites and text
+        arcade.draw_texture_rect(self.background, rect=arcade.LBWH(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+        # sets the background image
 
     def on_update(self, delta_time):
+        # this is automatically called many times per second.
+        # delta_time is how much time has passed since the last frame.
+        # mostly used for animations/movement
+        # you would add game logic here, such as update
         pass
+
+    # will need to add some more functions such as on_mouse_press() or on_mouse_drag() to support dragging.
 
 if __name__ == "__main__":
     game = MyGame()
